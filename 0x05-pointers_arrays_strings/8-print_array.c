8a37439b6a706a4c19d22d172f3eccd1da24e8f9
BukonlaDawodu <dawodubukola2000@gmail.com>
2023-05-02 16:37:16 -0700

    task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -3,18 +3,21 @@
 
 
 /**
- * print_array - prints n elements of an array
- * @a: array
- * @n: number of values to be printed
+ * print_array - Print n elements of an array of integers
+ * @a: Array of integers
+ * @n: Number of elements of the array to be printed
+ * Return: void 
  */
-void print_array(int *a, int n)
-{
-	int t;
 
-	for (t = 0; t < n; t++)
+void print_array(int *a, int n)
+
+{
+	int j;
+
+	for (j = 0; j < n; j++)
 	{
-	printf("%d", a[t]);
-	if (t != (n - 1))
+	printf("%d", a[j]);
+	if (j != (n - 1))
 	{
 	printf(", ");
 	}
